1e82917d140f4a590828a37c8c7c16bd3be86000
bandvov <bandvov@gmail.com>
2021-05-07 09:56:35 +0300

    added logger

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv'
+import log4js from 'log4js';
 import { app } from './app';
-
 dotenv.config();
+
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL as string;
+
+logger.info('log4js log info');
+logger.debug('log4js log debug');
+logger.error('log4js log error');
 
 const port = process.env.PORT || 5000
 
