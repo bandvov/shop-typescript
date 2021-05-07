1397b3c54e70c680061995cc5c53f74b41a6f217
bandvov <bandvov@gmail.com>
2021-05-07 21:08:17 +0300

    :sparkles::wrench: added eslint

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import log4js from 'log4js';
 import { app } from './app';
+
 dotenv.config();
 
 const logger = log4js.getLogger();
@@ -10,8 +11,8 @@ logger.info('log4js log info');
 logger.debug('log4js log debug');
 logger.error('log4js log error');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log('app is running on port',port); 
-})
+app.listen(port, () => {
+  console.log('app is running on port', port);
+});
