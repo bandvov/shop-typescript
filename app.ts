350bb634945b4bac14d2e6467b3022d396356605
bandvov <bandvov@gmail.com>
2021-05-08 11:31:02 +0300

    :wrench::passport_control:added cookie for user login

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
+app.disable('x-powered-by');
 app.get('/', (request, response) => {
   response.send('Hello world!');
 });
