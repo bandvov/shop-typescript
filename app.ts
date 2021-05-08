cf0c24058c2eeea884deca866c203766aa5e3a4b
bandvov <bandvov@gmail.com>
2021-05-08 20:45:38 +0300

    added swagger

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import userRouter from './routes/user';
+import * as swaggerDocument from './swagger/openapi.json';
 
 export const app = express();
 app.use(cors());
@@ -8,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 app.disable('x-powered-by');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (request, response) => {
   response.send('Hello world!');
 });
