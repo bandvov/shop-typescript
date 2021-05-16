36264ff3a2c9e220fabb6baa0289d1757f54237a
bandvov <bandvov@gmail.com>
2021-05-16 10:13:35 +0300

    :memo: updated swagger documentation

--- db.ts
+++ db.ts
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 const {
-  NODE_ENV, MONGO_USER, MONGO_PASSWORD, MONGO_DB, TEST_MONGO_DB,
+  NODE_ENV, MONGO_USER, MONGO_PASSWORD, MONGO_DB, MONGO_TEST_DB,
 } = process.env;
 const uri: string = NODE_ENV === 'UNITTEST'
-  ? `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.f8dbj.azure.mongodb.net/${TEST_MONGO_DB}?retryWrites=true&w=majority`
+  ? `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.f8dbj.azure.mongodb.net/${MONGO_TEST_DB}?retryWrites=true&w=majority`
   : `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.f8dbj.azure.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
