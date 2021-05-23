1c545b6a7d1fa514c2097e6d89c9f4cad90ba182
bandvov <bandvov@gmail.com>
2021-05-23 21:29:24 +0300

    :wrench: added constants

---
+++ client/src/configs/constants.ts
@@ -0,0 +1,6 @@
+export const BASIC_BACKGROUND_COLOR = '#E6EFF8';
+export const ERROR_MESSAGE_COLOR = 'ff0000';
+export const DARK_OUT_SHADOW = '6px 6px 8px 0px rgba(0,0,0,0.25)';
+export const DARK_INSET_SHADOW = 'inset 1px 1px 2px 0px rgba(0,0,0,0.25)';
+export const LIGHT_OUT_SHADOW = '6px 6px 8px 0px rgb(255,255,255)';
+export const lIGHT_INSET_SHADOW = 'inset 1px 1px 2px 0px rgb(255,255,255)';
