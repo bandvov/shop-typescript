acba28b8b542662bdd6af521ed8b5243f5cf493d
bandvov <bandvov@gmail.com>
2021-05-23 21:32:33 +0300

    added styled-components :heavy_plus_sign:

---
+++ client/src/styled.d.ts
@@ -0,0 +1,37 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background?: string;
+    width?:string;
+    minHeight?: string;
+    darkOutShadow?: string;
+    lightOutShadow?: string;
+    darkInsetShadow?: string;
+    lightInsetShadow?: string;
+    borderRadius?:{
+       primary?: string;
+       secondary?: string;
+    }
+    error?: string;
+    fontSize:{
+        small?:string;
+        normal?:string;
+    }
+    colors: {
+        button:{
+           background?:{
+             primary?:  string;
+             secondary?: string;
+           }
+           border?:{
+               primary: string;
+           } 
+           color?:{
+              primary: string;
+           } 
+         
+        }
+    };
+  }
+}
