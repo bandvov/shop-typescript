d239ac9f4ef285d55bca80a507fe0ed71b5aee11
bandvov <bandvov@gmail.com>
2021-06-04 10:00:01 +0300

    added header

---
+++ client/src/components/registration.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { StyledDiv } from './styled/styled-div';
+
+export const StyledRegistration = styled(StyledDiv)`
+  box-shadow: ${(props) => props.theme.darkOutShadow},
+    ${(props) => props.theme.lightOutShadow};
+  padding: ${(props) => props.padding || ''};
+`;
+function Registration(props: any): React.ReactElement {
+  return <StyledRegistration {...props} />;
+}
+export default Registration;
