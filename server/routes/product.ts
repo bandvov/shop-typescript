06638b89e616cd83870c0b7a68c5455c382c4723
bandvov <bandvov@gmail.com>
2021-06-06 16:39:25 +0300

    added product routes

--- server/routes/product.ts
+++ server/routes/product.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import { catchErrors } from '../handlers/error';
-import { ADD_PRODUCT_ROUTE, GET_PRODUCT_ROUTE } from '../configs';
-import { addProduct, getProduct } from '../controllers/product';
+import {
+  ADD_PRODUCT_ROUTE,
+  GET_PRODUCT_ROUTE,
+  GET_CATALOG_PRODUCTS_ROUTE,
+} from '../constants';
+import {
+  addProduct,
+  getProduct,
+  getProductsForCatalog,
+} from '../controllers/product';
 
 const router = express.Router();
 
 router.post(ADD_PRODUCT_ROUTE, catchErrors(addProduct));
 router.get(GET_PRODUCT_ROUTE, catchErrors(getProduct));
+router.get(GET_CATALOG_PRODUCTS_ROUTE, catchErrors(getProductsForCatalog));
 
 export default router;
