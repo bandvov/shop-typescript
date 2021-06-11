5fdfafb7e4e33c47263f34a7daba3e5cffd14bbd
bandvov <bandvov@gmail.com>
2021-06-11 09:52:46 +0300

    refactored routes

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -17,6 +17,7 @@ export const LOGIN_PATH = '/login';
 export const PROFILE_PATH = '/profile';
 export const REGISTER_PATH = '/register';
 export const WISHLIST_PATH = '/wishlist';
+export const PRODUCT_DETAILS_PATH = '/:id';
 
 export const EMAIL_REGEXP =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
