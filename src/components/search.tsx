819d3a46ce60df2badba90d864d46d6783f9f76a
bandvov <bandvov@gmail.com>
2021-06-13 17:02:39 +0300

    refactored search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,29 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Div from './common/div';
 import SearchPanel from './searchpanel';
 import SearchbarCTAContainer from '../containers/searchbar-cta-container';
 import logo from '../images/LOGO.svg';
-import { searchProducts } from './API';
 
-function Search(props:any):React.ReactElement {
-  console.log(props.match);
-  
-    const [searchQuery, setSearchQuery] = useState({});
-    useEffect(()=>{
-    
-      searchProducts(searchQuery).then((res)=>{
-        console.log(res);    
-      });
-    
-    },[searchQuery]);
+function Search():React.ReactElement {
 
-    
+
     return (
         <Div background="primary" justify="space-between" align="center" padding="1rem 0" width="85vw">
         <div style={{ height: '100%' }}>
           <img src={logo} />
         </div>
-        <SearchPanel onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setSearchQuery(e.target.value)} margin="0 0 0 1rem" width="100%" height="50px" />
+        <SearchPanel margin="0 0 0 1rem" width="100%" height="50px" />
         <SearchbarCTAContainer />
       </Div>
     );
--- src/components/searchpanel.tsx
+++ src/components/searchpanel.tsx
@@ -1,7 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {useState, useCallback } from 'react';
 import styled from 'styled-components';
 import Div from './common/div';
-import { faSave } from '@fortawesome/free-regular-svg-icons';
+import searchIcon from '../images/search-icon.svg';
+import { searchProducts } from './API';
 
 const StyledInput = styled.input<IProps>`
   margin: ${(props) => props.margin};
@@ -16,15 +17,34 @@ const StyledInput = styled.input<IProps>`
   box-shadow: ${(props) => props.theme.lightInsetShadow},
     ${(props) => props.theme.darkInsetShadow};
 `;
+const StyledSearchImage = styled.img<IProps>`
+position: absolute;
+left: 15px;
+border-radius: 50% 0 0 50%;
+padding: 1rem .5rem 1rem 1rem;
+cursor: pointer;
+`;
 
 function SearchPanel(props: IProps): React.ReactElement {
+
+const [search,setSearch] = useState<string>('');        
+  
+  const searchHandler =  (value:string)=>{
+
+    if (value === '') return;
+
+    searchProducts(value).then((res)=>{
+      console.log('search response',res);    
+    }).catch(e=>{
+      console.log(e);
+    });
+};
   return (
-    <Div padding={'0'} width="100%" position="relative" borderRadius="35px">
-      <FontAwesomeIcon
-        style={{ position: 'absolute', top: '18px', left: '35px' }}
-        icon={faSave}
+    <Div padding={'0'} width="100%" height='100%' position="relative" borderRadius="35px">
+      <StyledSearchImage onClick={()=>searchHandler(search)} aria-label='Click to search' title='Click to search'   
+        src={searchIcon}
       />
-      <StyledInput {...props} />
+      <StyledInput onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setSearch(e.target.value)} {...props} />
     </Div>
   );
 }
